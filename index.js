a6a5b363b7846e449910916facc84b5ee0f00f7c
kidcat8288 <kamea8288@icloud.com>
2026-09-13 19:35:59 -0400

    task 3 finished

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ function make_capital(sentence) {
 
 console.log(make_capital("you are my sunshine"));
 
+
+
+
+
+
 function max(num1, num2, num3) {
   let largest_number = num1;
 
@@ -18,6 +23,26 @@ function max(num1, num2, num3) {
   if (num3 > largest_number) {
     largest_number = num3;
   }
-  return  largest_number;
+  return largest_number;
 }
-console.log(max(100,50,80));
+console.log(max(100, 50, 80));
+
+
+
+
+
+function right(string) {
+  if (string.length < 3) {
+    return string;
+  }
+
+  let last_3 = string.substr(-3);
+  let rest = string.slice(0, -3);
+
+  let new_string = last_3 + rest;
+
+  return new_string;
+}
+
+
+console.log(right("school"))
