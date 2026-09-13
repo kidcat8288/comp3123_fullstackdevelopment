c202c81e93d3682a7d2ec1f5e62c09961f962249
kidcat8288 <kamea8288@icloud.com>
2026-09-13 19:16:04 -0400

    task 2 done

--- index.js
+++ index.js
@@ -9,10 +9,15 @@ function make_capital(sentence) {
 
 console.log(make_capital("you are my sunshine"));
 
+function max(num1, num2, num3) {
+  let largest_number = num1;
 
-
-
-
-
-
-
+  if (num2 > largest_number) {
+    largest_number = num2;
+  }
+  if (num3 > largest_number) {
+    largest_number = num3;
+  }
+  return  largest_number;
+}
+console.log(max(100,50,80));
